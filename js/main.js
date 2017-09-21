15d64d90c10f533aec0f32b81302b6d459b5fc96
Ashugeo <hugo.saintemarie@gmail.com>
2017-09-21 14:28:23 +0200

    Use data-attributes

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
-const letters = ['-', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
-
 $(document).ready(() => {
+    // Build the grid
+    buildGrid();
+});
+
+/**
+* Build the grid
+*/
+function buildGrid() {
+    const letters = ['-', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+
     for (let i = 0; i <= 10; i += 1) {
         $('.grid').append('<div class="row"></div>');
         for (let j = 0; j <= 10; j += 1) {
             if (j === 0) {
-                $('.row').eq(i).append('<div class="cell-' + i + '-' + j + ' locked">' + i + '</div>');
+                $('.row').eq(i).append('<div class="cell locked" data-row="' + i + '" data-col="' + j + '">' + i + '</div>');
             } else if (i === 0) {
-                $('.row').eq(i).append('<div class="cell-' + i + '-' + j + ' locked">' + letters[j] + '</div>');
+                $('.row').eq(i).append('<div class="cell locked" data-row="' + i + '" data-col="' + j + '">' + letters[j] + '</div>');
             } else {
-                $('.row').eq(i).append('<div class="cell-' + i + '-' + j + '"></div>');
+                $('.row').eq(i).append('<div class="cell" data-row="' + i + '" data-col="' + j + '"></div>');
+
             }
         }
     }
-});
+}
